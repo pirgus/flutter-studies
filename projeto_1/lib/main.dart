7172e55c237ca59aa1bb4ddaeeeb7c351ac7aa21
pirgus <jaquecavaller@gmail.com>
2023-01-04 16:28:50 -0300

    Evolução para um aplicativo contador simples

--- projeto_1/lib/main.dart
+++ projeto_1/lib/main.dart
@@ -1,14 +1,79 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Container(
-    child: Center(
-      child: Text(
-        'Hello, World!',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontSize: 30.0, color: Color.fromARGB(255, 255, 255, 255)),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Contador', home: MyHomePage());
+
+    throw UnimplementedError();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Aplicativo contador'),
+        backgroundColor: Colors.amber,
       ),
-    ),
-  ));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Vezes que você apertou o botão flutuante:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.displaySmall,
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Incrementar botão',
+        onPressed: _incrementCounter,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.amber,
+      ),
+      bottomNavigationBar: BottomAppBar(
+          color: Colors.amber,
+          child: Container(
+            height: 80,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                      onPressed: () {
+                        print('Adicionei alguma coisa');
+                      },
+                      icon: Icon(Icons.add_box, color: Colors.white)),
+                  IconButton(
+                    icon: Icon(Icons.add_a_photo, color: Colors.white),
+                    onPressed: () {
+                      print('Adicionei uma foto');
+                    },
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+
+    throw UnimplementedError();
+  }
 }
