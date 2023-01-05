37b0add6cf41ac7d59f02ba69ec4463e737e97a8
pirgus <jaquecavaller@gmail.com>
2023-01-05 11:15:56 -0300

    Utilizando PageView() e bottomNavBar reativa

--- app_basico/lib/pages/home_page.dart
+++ app_basico/lib/pages/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController _pageController = PageController();
+  int indexBottomNavigation = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,64 +17,87 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Center(child: Text('Aplicação Básica')),
       ),
-      body: Column(
+      body: PageView(
+        controller: _pageController,
         children: [
-          Container(
-            margin: const EdgeInsets.all(10),
-            height: 100,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                color: Colors.deepPurpleAccent,
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: Center(
-              child: Text(
-                'Olá, mundo!',
-                style: TextStyle(color: Colors.white, fontSize: 30),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 143, 106, 246),
-                      borderRadius: BorderRadius.all(Radius.circular(20))),
-                  height: 100,
-                  width: MediaQuery.of(context).size.width / 2 - 16.0,
-                  child: Center(
-                      child: Text(
-                    'Primeiro container',
-                    style: TextStyle(color: Colors.white),
-                  )),
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: 100,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurpleAccent,
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Center(
+                  child: Text(
+                    'Olá, mundo!',
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 143, 106, 246),
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    height: 100,
-                    width: MediaQuery.of(context).size.width / 2 - 16.0,
-                    child: Center(
-                        child: Text(
-                      'Segundo container',
-                      style: TextStyle(color: Colors.white),
-                    ))),
-              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 143, 106, 246),
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width / 2 - 16.0,
+                      child: Center(
+                          child: Text(
+                        'Primeiro container',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 143, 106, 246),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        height: 100,
+                        width: MediaQuery.of(context).size.width / 2 - 16.0,
+                        child: Center(
+                            child: Text(
+                          'Segundo container',
+                          style: TextStyle(color: Colors.white),
+                        ))),
+                  ),
+                ],
+              )
             ],
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.yellow,
           )
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined), label: 'Acessar conta'),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.add_a_photo_rounded), label: 'Adicionar foto')
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: indexBottomNavigation,
+          onTap: (int index) {
+            setState(() {
+              indexBottomNavigation = index;
+            });
+            _pageController.animateToPage(index,
+                duration: Duration(milliseconds: 300), curve: Curves.ease);
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), label: 'Acessar conta'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add_a_photo_rounded), label: 'Adicionar foto')
+          ]),
     );
   }
 }
