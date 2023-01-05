46675a31a6e39b7860587be09826e63b4cb3ece7
pirgus <jaquecavaller@gmail.com>
2023-01-05 17:16:39 -0300

    Personalização da aplicação de tarefas

--- todolist_app/lib/pages/todo_page.dart
+++ todolist_app/lib/pages/todo_page.dart
@@ -11,7 +11,8 @@ class ToDoPage extends StatefulWidget {
 
 class _ToDoPageState extends State<ToDoPage> {
   TextEditingController _textEditingController = TextEditingController();
-  List<String> tarefas = [];
+  List<String> tarefas = [], tarefas_finalizadas = [];
+  Color _tileColor = Colors.transparent;
 
   @override
   Widget build(BuildContext context) {
@@ -23,22 +24,90 @@ class _ToDoPageState extends State<ToDoPage> {
         padding: EdgeInsets.all(10),
         child: Column(
           children: [
+            Container(
+              margin: EdgeInsets.all(5.0),
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                  color: Colors.pink[400],
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(
+                      'Adicione uma tarefa que precisa ser realizada',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        wordSpacing: 2,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Text(
+                      'Mantenha pressionado sobre uma tarefa para finalizá-la',
+                      style: TextStyle(fontSize: 12, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             TextField(
               controller: _textEditingController,
             ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Tarefas ainda não finalizadas',
+              ),
+            ),
             Container(
-              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.pink[200],
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              height: MediaQuery.of(context).size.height * 0.25,
               child: ListView.separated(
-                  separatorBuilder: (context, index) => Divider(),
+                  separatorBuilder: (context, index) => Divider(
+                        color: Colors.black,
+                      ),
                   itemCount: tarefas.length,
                   itemBuilder: ((context, index) {
                     return ListTile(
+                      focusColor: _tileColor,
                       title: Text(tarefas[index]),
                       onLongPress: () {
                         setState(() {
+                          tarefas_finalizadas.add(tarefas[index]);
                           tarefas.removeAt(index);
                         });
                       },
+                    );
+                  })),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Tarefas finalizadas',
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.pink[100],
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: ListView.separated(
+                  separatorBuilder: (context, index) => Divider(
+                        color: Colors.black,
+                      ),
+                  itemCount: tarefas_finalizadas.length,
+                  itemBuilder: ((context, index) {
+                    return ListTile(
+                      focusColor: _tileColor,
+                      title: Text(tarefas_finalizadas[index]),
                     );
                   })),
             ),
@@ -73,11 +142,12 @@ class _ToDoPageState extends State<ToDoPage> {
                 // pelo menos um caractere digitado
                 setState(() {
                   tarefas = [];
+                  tarefas_finalizadas = [];
                 });
                 _textEditingController.clear();
               },
               backgroundColor: Colors.pink[700],
-              child: Icon(Icons.remove),
+              child: Icon(Icons.delete_forever_rounded),
             ),
           ),
         ],
